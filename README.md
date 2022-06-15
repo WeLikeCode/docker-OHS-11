5fdf500eaf5da649dd7ba21b160c9726860eb389
CiprianSpot <ciprian@spotus.space>
2022-06-16 00:46:05 +0300

    imit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # docker-OHS-11
 Oracle Apache 2 version 
+
+# Inspired by huhgawz
+
+https://github.com/huhgawz/docker-oraclelinux-ohs
+
---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#su - /bin/bash oracle -c "cd /oracle/Middleware/Oracle_WT1/opmn/bin && ./opmnctl startall"
+
+/bin/bash -c "cd /oracle/Middleware/Oracle_WT1/opmn/bin && ./opmnctl startall"
+
+sleep infinity
